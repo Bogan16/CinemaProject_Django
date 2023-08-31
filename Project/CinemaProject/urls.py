a65a899a539e1e82be565357c319f093a5af272f
Bogan16 <bogdan66334@gmail.com>
2023-08-31 22:05:21 +0300

    Added additional urls

--- Project/CinemaProject/urls.py
+++ Project/CinemaProject/urls.py
@@ -21,5 +21,6 @@ from cinemaapp.views import HomeView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
-    path('api/', include('cinemaapp.urls')), 
+    path('', include('cinemaapp.urls')),
+    path('api/', include('cinemaapp.urls')),  
 ]
